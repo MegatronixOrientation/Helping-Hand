e2a6f089bc568256cde35e20a3f61c6c1134b1b5
Atul Singh <cocatul11@gmail.com>
2023-09-26 18:12:59 +0530

    first commit

---
+++ helpinghand.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ff1ce7466b1689a26a9f7db961634dcaa838b28c23f6f7e3f2f9e88100efbb24
+size 13427
---
+++ helpinghand.spec
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:76787b4fe4abb79a219f709bfd0dd6d5aa63bc59102b445235c163931aa450d2
+size 867
